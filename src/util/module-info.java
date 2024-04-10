6d925e7c7c6bb895fb618a7a2c687de2cd88e251
Darsh Chaurasia <darshchaurasia@Darshs-MacBook-Pro-2.local>
2024-04-09 23:45:00 -0700

    updated version with working patientlist ui

--- src/util/module-info.java
+++ src/util/module-info.java
@@ -2,10 +2,12 @@ module TH43 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+	requires javafx.graphics;
 
     // If you're using any additional modules, list them here with 'requires'
 
     // This opens the packages to javafx.graphics module, necessary for reflection
+	opens model to javafx.base;
     opens ui to javafx.graphics, javafx.fxml;
     exports ui;
 }
